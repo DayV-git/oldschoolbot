c61f206829260b21f0c84c9972814a00f1ddc3e0
DayV <ashleyjd22@gmail.com>
2025-05-27 20:24:54 +0100

    File moveover

--- src/lib/util/repeatStoredTrip.ts
+++ src/lib/util/repeatStoredTrip.ts
@@ -51,6 +51,7 @@ import type {
 	RunecraftActivityTaskOptions,
 	SawmillActivityTaskOptions,
 	ScatteringActivityTaskOptions,
+	SepulchreActivityTaskOptions,
 	ShadesOfMortonOptions,
 	SmeltingActivityTaskOptions,
 	SmithingActivityTaskOptions,
@@ -539,8 +540,12 @@ const tripHandlers = {
 	},
 	[activity_type_enum.Sepulchre]: {
 		commandName: 'minigames',
-		args: () => ({ sepulchre: { start: {} } })
+		args: (data: SepulchreActivityTaskOptions) => {
+			const fletch = data.fletch?.fletchableName;
+			return { sepulchre: { start: { fletching: fletch } } };
+		}
 	},
+
 	[activity_type_enum.Smithing]: {
 		commandName: 'smith',
 		args: (data: SmithingActivityTaskOptions) => ({
